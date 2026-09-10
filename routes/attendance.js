88598a8b7bc78f4cfc78d81d57945246fae80270
Gh1y4th <ghiyath2011hk@gmail.com>
2026-09-10 10:18:31 +0300

    Update attendance.js

--- routes/attendance.js
+++ routes/attendance.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const { getDb } = require('../firestore');
 const { verifyFirebaseToken, requireDbUser, verifyPythonServiceKey } = require('../middleware/auth');
 
+function getDayRange(date) {
+  const start = new Date(date);
+  start.setHours(0, 0, 0, 0);
+
+  const end = new Date(date);
+  end.setHours(23, 59, 59, 999);
+
+  return { start, end };
+}
+
 router.get('/', verifyFirebaseToken, requireDbUser, async (req, res) => {
   const db = getDb();
   try {
